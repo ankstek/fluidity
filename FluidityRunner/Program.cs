3b58b5aea16141a037b4a9b1c14f6fb038094c16
Chris <kqr@users.noreply.github.com>
2019-10-18 11:49:05 +0200

    Fix L1 neighbour mean computation (#14)
    This used to throw an IndexError (as well as go on forever, due to the
    postincrement operator in the recursive call) – I hope I did the right thing to it!

--- FluidityRunner/Program.cs
+++ FluidityRunner/Program.cs
@@ -7,6 +7,7 @@ namespace Fluidity.Runner
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            new Core.MotionEngine().run();
         }
     }
 }
